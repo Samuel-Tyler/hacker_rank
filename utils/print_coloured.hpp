c2b4dc6d6ded991b31970f36c421ddae5fa5239e
Sam <samtyler6@gmail.com>
2018-04-09 21:00:01 +0100

    Update utilities - printing

---
+++ utils/print_coloured.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#ifdef __linux__
+enum class Console_colour : int {
+    FG_DEFAULT = 39,
+    FG_BLACK = 30,
+    FG_RED = 31,
+    FG_GREEN = 32,
+    FG_YELLOW = 33,
+    FG_BLUE = 34,
+    FG_MAGENTA = 35,
+    FG_CYAN = 36,
+    FG_LIGHT_GRAY = 37,
+    FG_DARK_GRAY = 90,
+    FG_LIGHT_RED = 91,
+    FG_LIGHT_GREEN = 92,
+    FG_LIGHT_YELLOW = 93,
+    FG_LIGHT_BLUE = 94,
+    FG_LIGHT_MAGENTA = 95,
+    FG_LIGHT_CYAN = 96,
+    FG_WHITE = 97,
+    BG_RED = 41,
+    BG_GREEN = 42,
+    BG_BLUE = 44,
+    BG_DEFAULT = 49
+};
+
+void print_coloured(const std::string& info, Console_colour colour)
+{
+    std::cout << "\033[" << static_cast<int>(colour) << "m" <<info << "\033[" << static_cast<int>(Console_colour::FG_DEFAULT) << 'm';
+}
+#elif _WIN32
+#include <windows.h>
+
+enum class Console_colour : WORD {
+    //   FG_DEFAULT
+    FG_BLACK = FOREGROUND_INTENSITY,
+    FG_RED = FOREGROUND_INTENSITY | FOREGROUND_RED,
+    FG_GREEN = FOREGROUND_INTENSITY | FOREGROUND_GREEN,
+    FG_YELLOW = FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_RED,
+    FG_BLUE = FOREGROUND_INTENSITY | FOREGROUND_BLUE,
+    FG_MAGENTA = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_RED,
+    FG_CYAN = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN,
+    FG_WHITE = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED,
+    FG_LIGHT_RED = FOREGROUND_RED,
+    FG_LIGHT_GREEN = FOREGROUND_GREEN,
+    FG_LIGHT_YELLOW = FOREGROUND_GREEN | FOREGROUND_RED,
+    FG_LIGHT_BLUE = FOREGROUND_BLUE,
+    FG_LIGHT_MAGENTA = FOREGROUND_BLUE | FOREGROUND_RED,
+    FG_LIGHT_CYAN = FOREGROUND_BLUE | FOREGROUND_GREEN,
+};
+
+void print_coloured(const std::string& info, Console_colour colour)
+{
+    WORD Attributes = 0;
+    CONSOLE_SCREEN_BUFFER_INFO Info;
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(hStdout, &Info);
+    Attributes = Info.wAttributes;
+    SetConsoleTextAttribute(hStdout, static_cast<WORD>(colour));
+    std::cout << info;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Attributes);
+}
+
+#else
+
+enum class Console_colour : int {
+    FG_DEFAULT = 39,
+    FG_BLACK = 30,
+    FG_RED = 31,
+    FG_GREEN = 32,
+    FG_YELLOW = 33,
+    FG_BLUE = 34,
+    FG_MAGENTA = 35,
+    FG_CYAN = 36,
+    FG_LIGHT_GRAY = 37,
+    FG_DARK_GRAY = 90,
+    FG_LIGHT_RED = 91,
+    FG_LIGHT_GREEN = 92,
+    FG_LIGHT_YELLOW = 93,
+    FG_LIGHT_BLUE = 94,
+    FG_LIGHT_MAGENTA = 95,
+    FG_LIGHT_CYAN = 96,
+    FG_WHITE = 97,
+    BG_RED = 41,
+    BG_GREEN = 42,
+    BG_BLUE = 44,
+    BG_DEFAULT = 49
+};
+
+void print_coloured(const std::string& info, Console_colour)
+{
+    std::cout << info;
+}
+#endif
--- utils/print_container.hpp
+++ utils/print_container.hpp
@@ -1,38 +1,43 @@
+#pragma once
+
 #include <iostream>
 #include <map>
 
 template <typename T1, typename T2>
-void print(std::pair<T1, T2>  pair)
+void print(const std::pair<T1, T2>& pair)
 {
-   std::cout << pair.first << " : " << pair.second << std::endl;
+  std::cout << "{ " <<  pair.first << " : " << pair.second << " }";
 }
 
-template <typename T1, typename T2>
-void print(std::map<T1, T2> container)
+template <typename T1, typename T2, typename T3>
+void print(const std::map<T1, T2, T3>& container)
 {
-    int i = 0;
-    for (const auto& obj : container)
-    {
-        std::cout << i++ << " : " << obj.first << " : " << obj.second << std::endl;
-    }
+  std::cout << "{ ";
+  for (const auto& obj : container)
+  {
+    print(obj);
+    std::cout << ", ";
+  }
+  std::cout << "}\n";
 }
 
-template <typename T1, typename T2>
-void print(std::multimap<T1, T2> container)
+template <typename T1, typename T2, typename T3>
+void print(const std::multimap<T1, T2, T3>& container)
 {
-    int i = 0;
-    for (const auto& obj : container)
-    {
-        std::cout << i++ << " : " << obj.first << " : " << obj.second << std::endl;
-    }
+  int i = 0;
+  for (const auto& obj : container)
+  {
+    std::cout << i++ << " : " << obj.first << " : " << obj.second << std::endl;
+  }
 }
 
 template <typename T>
-void print(T container)
+void print(const T& container)
 {
-    int i = 0;
-    for (const auto& obj : container)
-    {
-        std::cout << i++ << " : " << obj <<std::endl;
-    }
-}
+  std::cout << "{ ";
+  for (const auto& obj : container)
+  {
+    std::cout << obj << ' ';
+  }
+  std::cout << "}\n";
+} 
